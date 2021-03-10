779a50353d1b498752ead697d863a367308e9e76
HOME <Kanova2004@yandex.ru>
2021-03-10 21:38:48 +0500

    гена... ты просил меня принести тебе полотенце... я пошел в спальню... а там... читать продолжение...

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -26,6 +26,21 @@ def mars_img():
 @app.route('/promotion_image')
 def promotion_image():
     return render_template("моя маленькая американская бейба.html")
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def astronaut_selection():
+    if request.method == 'GET':
+        return render_template("вова сказал, что АЮ не позер и за все шарит. ауф.html")
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['class'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
